e79905d8912ef8ecc02fda600b339a8bb681547e
biarmortari <beatrizrmortari@gmail.com>
2025-11-06 11:59:12 -0300

    feat(users): adicionar rota GET /users para listar usuários

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ const app = express();
 app.listen(PORT, () => {
   console.log(`O app está executando na porta ${PORT}`);
 });
+
+app.get("/users/:id", (req, res) => {
+  if (!users[req.params.id]) {
+    res.status(404).send("ID do usuário não encontrado");
+    return;
+  }
+  res.send(users[req.params.id]);
+});
