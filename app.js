30c0fd115536da6f3b32c3e71fba1d0acf1dfcab
biarmortari <beatrizrmortari@gmail.com>
2025-11-24 11:43:28 -0300

    feat(mongoose): cria modelo para esquema de usuário

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 const { PORT = 3000 } = process.env;
+
+mongoose.connect("mongodb://localhost:27017/aroundb");
 
 const userRoute = require("./routes/users");
 
