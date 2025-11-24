6d248218ce9370a2fb6e552cdaa1fa1610aa5dd5
biarmortari <beatrizrmortari@gmail.com>
2025-11-24 11:58:24 -0300

    feat(mongoose): cria o modelo para o esquema de cards

---
+++ models/card.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^https?:\/\/(www\.)?[a-zA-Z0-9-]+\.[a-zA-Z0-9-]{2,}(\/[a-zA-Z0-9._~:/?%#\[\]@!$&'()*+,;=-]+)*\/?#?$/.test(
+          v
+        );
+      },
+      message: (props) => `${props.value} não é uma URL válida`,
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  likes: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "User",
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("card", cardSchema);
