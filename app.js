b10d1a8cc437303ecb8d59addc85b8c7ec216363
biarmortari <beatrizrmortari@gmail.com>
2025-11-25 17:09:19 -0300

    refactor: refatora a rota dos usuários e conecta com o controles/modelos

--- app.js
+++ app.js
@@ -2,22 +2,27 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+app.use(express.json());
+
 const { PORT = 3000 } = process.env;
 
 mongoose.connect("mongodb://localhost:27017/aroundb");
 
 const userRoute = require("./routes/users");
-
 const cardsRoute = require("./routes/cards");
 
-app.listen(PORT, () => {
-  console.log(`O app está executando na porta ${PORT}`);
-});
-
 app.use("/users", userRoute);
-
 app.use("/cards", cardsRoute);
+
+app.post("/debug", (req, res) => {
+  console.log("BODY RECEBIDO:", req.body);
+  res.send({ body: req.body });
+});
 
 app.use((req, res) => {
   res.status(404).send({ message: "A solicitação não foi encontrada" });
+});
+
+app.listen(PORT, () => {
+  console.log(`O app está executando na porta ${PORT}`);
 });
--- routes/users.js
+++ routes/users.js
@@ -1,36 +1,11 @@
-const fs = require("fs");
-const path = require("path");
 const router = require("express").Router();
 
-const usersPath = path.join(__dirname, "../data/users.json");
+const { getUsers, getUserById, createUser } = require("../controllers/users");
 
-router.get("/", (req, res) => {
-  fs.readFile(usersPath, "utf8", (err, data) => {
-    if (err) {
-      return res.status(500).send({ message: "Ocorreu um erro no servidor" });
-    }
+router.get("/", getUsers);
 
-    const users = JSON.parse(data);
-    res.send(users);
-  });
-});
+router.get("/:userId", getUserById);
 
-router.get("/:id", (req, res) => {
-  fs.readFile(usersPath, "utf8", (err, data) => {
-    if (err) {
-      return res.status(500).send({ message: "Ocorreu um erro no servidor" });
-    }
-
-    const users = JSON.parse(data);
-    const userId = req.params.id;
-    const userFound = users.find((user) => user._id === userId);
-
-    if (!userFound) {
-      return res.status(404).send({ message: "ID de usuário não encontrado" });
-    }
-
-    res.send(userFound);
-  });
-});
+router.post("/", createUser);
 
 module.exports = router;
