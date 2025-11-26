a01890255f7bb9fa3f967dd0e33792469c023184
biarmortari <beatrizrmortari@gmail.com>
2025-11-26 19:23:40 -0300

    fix(controllers): melhorar tratamento de erros e aplicar orFail na exclusão de cartões

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,33 +2,41 @@ const Card = require("../models/card");
 
 module.exports.getCards = (req, res) => {
   Card.find({})
-    .then((cards) => res.send({ cards }))
-    .catch((err) =>
-      res.status(500).send({ message: `Erro ao buscar cartões` })
-    );
+    .then((cards) => res.send(cards))
+    .catch(() => res.status(500).send({ message: `Erro ao buscar cartões` }));
 };
 
 module.exports.createCard = (req, res) => {
   console.log(req.user._id);
   const { name, link } = req.body;
 
-  if (!name || !link) {
-    return res
-      .status(400)
-      .send({ message: `O nome do cartão e a URL são obrigatórios` });
-  }
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send(card))
-    .catch((err) => res.status(500).send({ message: `Erro ao criar cartão` }));
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res
+          .status(400)
+          .send({ message: "Dados inválidos para criação do cartão" });
+      }
+      return res.status(500).send({ message: "Erro interno ao criar cartão" });
+    });
 };
 
 module.exports.deleteCard = (req, res) => {
-  Card.findByIdAndDelete(req.params.cardId).then((card) => {
-    if (!card) {
-      return res.status(404).send({ message: `Cartão não encontrado` });
-    }
-    res.send(card);
-  });
-  res.send({ data: card });
-  res.status(500).send({ message: `Erro ao deletar cartão` });
+  Card.findByIdAndDelete(req.params.cardId)
+    .orFail()
+    .then((card) => {
+      res.send(card);
+    })
+    .catch((err) => {
+      if (err.name === "CastError") {
+        return res.status(400).send({ message: "ID inválido" });
+      }
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(404).send({ message: "Card não encontrado" });
+      }
+      return res
+        .status(500)
+        .send({ message: "Error interno ao deletar cartão" });
+    });
 };
